bfb3505efc1f629c66cb950bb34fe29669badff8
Marcel <viehmaie@hs-pforzheim.de>
2020-11-24 17:33:05 +0100

    Initialized Project

---
+++ src/main/java/de/hspf/authservice/AuthserviceRestApplication.java
@@ -0,0 +1,13 @@
+package de.hspf.authservice;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+@ApplicationScoped
+public class AuthserviceRestApplication extends Application {
+}
---
+++ src/main/java/de/hspf/authservice/HelloController.java
@@ -0,0 +1,18 @@
+package de.hspf.authservice;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ *
+ */
+@Path("/hello")
+@Singleton
+public class HelloController {
+
+    @GET
+    public String sayHello() {
+        return "Hello World";
+    }
+}
