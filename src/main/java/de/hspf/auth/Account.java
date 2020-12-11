154bdb16c3daad2797af4603e00e093d29056148
Marcel <viehmaie@hs-pforzheim.de>
2020-12-11 17:48:48 +0100

    Added Generation Type for Account Entity

--- src/main/java/de/hspf/auth/Account.java
+++ src/main/java/de/hspf/auth/Account.java
@@ -7,6 +7,8 @@ package de.hspf.auth;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
@@ -21,6 +23,7 @@ import javax.persistence.Table;
 public class Account {
     
     @Id
+    @GeneratedValue(strategy=GenerationType.AUTO) 
     private int id;
     @Column
     private String username;
